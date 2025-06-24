486c43be948e70f702f9f963f5048191b90de5aa
Simone Salerno <doppiase@proton.me>
2025-06-25 00:00:13 +0300

    Update XML namespace in sitemap generation logic for consistency with sitemap standards.

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -249,7 +249,7 @@ const sitemap = (
 ) => `<?xml version="1.0" encoding="UTF-8" ?>
 <?xml-stylesheet type="text/css" href="/sitemap.css"?>
 <urlset
-  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:xhtml="http://www.w3.org/1999/xhtml"
 >
   ${pages
