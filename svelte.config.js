432de2175c44d2133b391ebb990f57dedde63080
Simone Salerno <doppiase@proton.me>
2025-08-03 10:23:40 +0200

    fix: ensure WASM files are included in the adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	kit: {
 		adapter: adapter({
+			includeFiles: ['**/*.wasm'],
 			routes: {
 				include: ['/*'],
 				exclude: ['<all>']
